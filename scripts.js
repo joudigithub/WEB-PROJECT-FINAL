470063495138bbf5383ddecec5c89c6aeeb61b83
MohammadAssemNasser <mhmd.asm.nsr@gmail.com>
2019-12-14 22:13:14 +0200

    form picking fixed

--- scripts.js
+++ scripts.js
@@ -1,10 +1,76 @@
+var value = {
+    standard: [{
+            id: 1,
+            name: 'Upper Deck (7,000 L.L.)',
+            value: 7000,
+        },
+        {
+            id: 2,
+            name: 'Lower Deck (3,000 L.L.)',
+            value: 3000,
+        }
+    ],
+    express: [{
+        id: 3,
+        name: 'Express (5,000 L.L.',
+        value: 5000,
+    }]
+}
+var trips = [{
+        id: 100,
+        name: '#100 BEY-TRI (EXPRESS)',
+        value: value.express,
+    },
+    {
+        id: 200,
+        name: '#200 BEY- SID (STANDARD)',
+        value: value.standard,
+    },
+    {
+        id: 300,
+        name: '#300 BEY-BYB (STANDARD)',
+        value: value.standard,
+    },
+    {
+        id: 400,
+        name: '#400 TYR-BEI (EXPRESS)',
+        value: value.express,
+    },
+]
+
+function onTripChange() {
+    var selectedTrip = $('#selecttrip').children("option:selected").val()
+
+    if (selectedTrip != '') {
+        $('#selectdeck').prop('disabled', false)
+        var options = '<option value="">Please Choose Your Deck</option>'
+        console.log(trips.find(trip => {
+            return trip.id == selectedTrip
+        }))
+        trips.find(trip => {
+            return trip.id == selectedTrip
+        }).value.forEach(type => {
+            options += `<option value="${type.value}">${type.name}</option>`
+        })
+        $('#selectdeck').html(options)
+    } else {
+        $('#selectdeck').prop('disabled', true)
+        $('#selectdeck').html('<option value="">Please Choose Your Trip First</option>')
+    }
+
+}
+
 $(document).ready(function() {
+    $('#selectdeck').prop('disabled', false)
 
     // init options
     (function() {
-        $('#start').html();
+        var tripOptions = '<option value="">Please Choose Your Trip</option>'
+        trips.forEach(trip => {
+            tripOptions += `<option value="${trip.id}">${trip.name}</option>`
+        })
+        $('#selecttrip').html(tripOptions)
     })();
-
 
     // MODALS
     var btn = $("#tickets-card");
